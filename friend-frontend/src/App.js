5efef24b144420120f43c2b8cebbc40ec2e93ca2
KartikAKGEC89 <s11kartik2111087@gmail.com>
2025-01-11 21:17:54 +0530

    Register form

--- friend-frontend/src/App.js
+++ friend-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './components/Home'
 import Login from './components/Login'
+import Register from './components/Register'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </BrowserRouter>
   )
